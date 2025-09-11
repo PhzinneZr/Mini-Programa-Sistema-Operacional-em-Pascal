7bd06425d9421afb70035a2f47b608d7e464d4b3
PhzinneZr <ph9429848@gmail.com>
2025-09-11 09:45:00 -0300

    Atualização - Paint Adicionado

--- MiniProgramaOSV.1.pas
+++ MiniProgramaOSV.1.pas
@@ -2,10 +2,9 @@ Program MeuMiniProgramaSimulado;
 uses crt;
 var
 	senha: string;
-	num, num2, opcoes, opcoes2, numcorreto, numEsc, tentativas, senhaerrada: integer;
-	
-	//Crio uma função ja definida para a calculadora.
-	
+	x,y,num, num2, opcoes, opcoes2, numcorreto, numEsc, tentativas, senhaerrada: integer;
+	tecla:char;
+
 	function Calculadora(num,num2,opcoes:integer): real;
 	Begin
 		case (opcoes) of
@@ -18,32 +17,26 @@ var
 	End;				
 
 Begin
-	
-	//Defino número tolerável para senhas erradas em 5 (definindo 0 no começo para ser contado no verificador), se o número de senhas erradas forem iguais a 5, a tentativa de loguin falha.
-	//Defino número de tentativas em 0 (Jogo de advinhar números), para que seja contado e exibido ao final do jogo. 
-	
+x:=20;
+y:=20;
 	tentativas:=0;
 	senhaerrada:=0;
 	
 	writeln('Seja Bem-Vindo PH!');
- 		delay(233);
+ 	delay(233);
  	
-	//Minha Seção de Login
-	
+	// --- Minha SeÃ§Ã£o de Login ---
 	repeat 		
 		writeln('Por favor insira sua senha!');
-			readln(senha);
+		readln(senha);
 		
-			//Verificação de senhas	
 		
 		if (senha <> 'Phworks2025') then
-			begin			
-				senhaerrada:=senhaerrada + 1; 
-				writeln('Senha incorreta.');
-		
-			//Verifica se o número de senhas erradas "5" é real.
+		begin			
+			senhaerrada:=senhaerrada + 1; 
+			writeln('Senha incorreta.');
 			
-		if (senhaerrada = 5) then			
+			if (senhaerrada = 5) then			
 			begin	
 				writeln('Tentativas demais ao acessar o programa, tente novamente mais tarde!');
 				writeln('Encerrando programa..........');
@@ -52,131 +45,126 @@ Begin
 				Exit; 			
 			end;		
 		end;	
-	
-	  //Fim do loop de repetição para verificar senhas.
-		
 	until(senha = 'Phworks2025');
 
-	writeln('Você logou!');
+	writeln('VocÃª logou!');
 	delay(430);
 	
-	//Loop Principal do Menu
+	// --- Loop Principal do Menu ---
 	repeat 
+  textbackground(black);
 	
-	clrscr;  //Adiciono um "clrscr" para limpar a tela a cada vez que escolhem oque quer fazer no sistema.
-		
+	clrscr;
 		writeln; 
-		writeln('O que irá fazer hoje?');
+		writeln('O que irÃ¡ fazer hoje?');
 		writeln('Digite 1 para abrir sua calculadora');
-		writeln('Digite 2 para abrir jogo do número secreto');
+		writeln('Digite 2 para abrir jogo do nÃºmero secreto');
+		writeln('Digite 3 para abrir seu Paint');
 		writeln('Digite 0 para fechar o programa e dar LogOff');
 		readln(opcoes2); 
 		
-		//Crio um Switch Case para cada opção que o usúario escolher 
-		
 		case (opcoes2) of
-			
-		//Calculadora
-			
 			1:	begin	
-						
-						clrscr;
-						
-						writeln('Bem-Vindo à calculadora! digite dois números e escolha o que fazer com eles');
-						writeln('Informe o primeiro número');
-						readln(num);
+			  clrscr;
+					writeln('Bem-Vindo Ã  calculadora! digite dois nÃºmeros e escolha o que fazer com eles');
+					writeln('Informe o primeiro nÃºmero');
+					readln(num);
 					
-						writeln('Informe o segundo número');
-						readln(num2);
+					writeln('Informe o segundo nÃºmero');
+					readln(num2);
 	
-						writeln('O que você quer fazer?');
-						delay(605);
+					writeln('O que vocÃª quer fazer?');
+					delay(605);
 					
-						writeln('1: Adição');
-						writeln('2: Divisão');
-						writeln('3: Subtração');
-						writeln('4: Multiplicação');
-						writeln('5: Raíz quadrada do primeiro número');
-						readln(opcoes);
+					writeln('1: AdiÃ§Ã£o');
+					writeln('2: DivisÃ£o');
+					writeln('3: SubtraÃ§Ã£o');
+					writeln('4: MultiplicaÃ§Ã£o');
+					writeln('5: RaÃ­z quadrada do primeiro nÃºmero');
+					readln(opcoes);
 					
-						writeln('Os resultados são: ' , Calculadora(num,num2,opcoes):0:2 );
-						delay(2000); 
-					end;
+					writeln('Os resultados sÃ£o: ' , Calculadora(num,num2,opcoes):0:2 );
+					delay(2000); 
+				end;
 				
 			2:	begin
-			
-			      //Game simples de advinhar números
-						
-						clrscr;
-						writeln('Boas Vindas, PH!');
-						writeln('Carregando seu jogo...');
-						delay(234);
-						writeln('Carregando....24%');
-						delay(342);
-						writeln('Carregando....49%');
-						delay(432);
-						writeln('Carregando....78%');
-						delay(123);
-						writeln('Carregando....100%');
-						delay(1234);
-						writeln('O jogo foi carregado com sucesso! iniciando.');
+			clrscr;
+					writeln('Boas Vindas, PH!');
+					writeln('Carregando seu jogo...');
+					delay(234);
+					writeln('Carregando....24%');
+					delay(342);
+					writeln('Carregando....49%');
+					delay(432);
+					writeln('Carregando....78%');
+					delay(123);
+					writeln('Carregando....100%');
+					delay(1234);
+					writeln('O jogo foi carregado com sucesso! iniciando.');
 					
-						writeln('Adivinhe o número e ao final irá exibir suas tentativas!');
-						writeln('Boa sorte!');
+					writeln('Adivinhe o nÃºmero e ao final irÃ¡ exibir suas tentativas!');
+					writeln('Boa sorte!');
 					
-						//Adiciono um número aleatorio para ser advinhado	
-				
-						numcorreto:= random(100) + 1; 
-						tentativas:= 0; 
+					numcorreto:= random(100) + 1; 
+					tentativas:= 0; 
 					
-						repeat
-							writeln('Digite o número que você acha que é o correto:');
-								readln(numEsc);
+					repeat
+						writeln('Digite o nÃºmero que vocÃª acha que Ã© o correto:');
+						readln(numEsc);
 						
-							if (numEsc > numcorreto) then
-								begin
-								writeln('O número é menor!');
-								tentativas:=tentativas + 1;
-							end
-						
-							else if (numEsc < numcorreto) then
-								begin
-								writeln('O número correto é maior');
-								tentativas:=tentativas + 1;
-							end
-						
-							else 
-								begin
-									writeln('Parabéns!!! Você acertou o número correto!');
-									writeln('Suas tentativas: ' , tentativas );
-							end;
-						
-							//O programa termina quando o usúario finalmente acerta o número.
-						
-						until(numEsc = numcorreto);
-						delay(2000); 
-					
-					end;
-				
-		    	//Sistema de encerramento do programa caso o usúario digite 0.
-				
-			0:	begin
-						writeln('Você decidiu sair para LogOff...');
-						writeln('Encerrando o programa...');
-						delay(1432);			
-					end;
-			
-				//Abaixo, sistema para exibir mensagem de opções inválidas e loop para continuar o programa.
+						if (numEsc > numcorreto) then
+						begin
+							writeln('O nÃºmero Ã© menor!');
+							tentativas:=tentativas + 1;
+						end
+						else if (numEsc < numcorreto) then
+						begin
+							writeln('O nÃºmero correto Ã© maior');
+							tentativas:=tentativas + 1;
+						end
+						else 
+						begin
+							writeln('ParabÃ©ns!!! VocÃª acertou o nÃºmero correto!');
+							writeln('Suas tentativas: ' , tentativas );
+						end;
+					until(numEsc = numcorreto);
+					delay(2000); 
+				end;
+			  3: begin 
+				clrscr;                                            
+			  writeln('Voce entrou no paint');
+			  writeln('Para dar break no programa digite "p"');
+			  writeln('Use os botÃµes de movimentos que sÃ£o os:');
+			  writeln
+			  writeln('		  w  ');
+			  writeln('		a-s-d');
+			  repeat
+				    tecla:=readkey;	 	 		 
+						case tecla of 
+						'w': x:= x - 1;     
+						's': x:= x + 1;        
+						'a': y:= y - 1;        
+						'd': y:= y + 1;  
+					  end;           
+				  gotoxy(y,x);
+				  textbackground(red);
+				writeln(' ');
+	
+				until(tecla='p');      
+				end;   
+			0: begin
+					writeln('VocÃª decidiu sair para LogOff...');
+					writeln('Encerrando o programa...');
+					delay(1432);			
+				end;
 			
 			else 
 				begin
-					writeln('Opção inválida! Por favor, digite 1, 2 ou 0.');
+					writeln('OpÃ§Ã£o invÃ¡lida! Por favor, digite 1, 2 ou 0.');  
 					delay(1000);
 				end;
-		  end;
-	
+		end; 
+		
 	until(opcoes2 = 0);
-	
-			//Por favor se você for usar este programa peço humildemente que deixe os creditos: ©PhzinneZr2025
 	
 End.
